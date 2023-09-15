c8e6c4c5d42bf6857bd5f5462f86d71a63c42417
Samuel Cheng <samuelc1231@gmail.com>
2023-09-16 01:03:52 +0800

    Initial Commit

---
+++ Memory Card/Memory_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_CardApp.swift
+//  Memory Card
+//
+//  Created by Samuel Cheng on 2023/9/16.
+//
+
+import SwiftUI
+
+@main
+struct Memory_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
